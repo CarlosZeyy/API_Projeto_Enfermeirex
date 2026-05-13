2f16f9836aa91b0917cb38c6f22d956707dfeb44
CarlosZeyy <carloszeeyy@gmail.com>
2026-05-13 19:43:48 -0300

    fix: add username in dto for get username in frontend

--- src/main/java/dev/carlosmoises/projeto/enferm/DTO/CreateUserDTO.java
+++ src/main/java/dev/carlosmoises/projeto/enferm/DTO/CreateUserDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record CreateUserDTO(@NotBlank String email, @NotBlank @Size(min = 6) String password,
+public record CreateUserDTO(@NotBlank String email, @NotBlank @Size(min = 6) String password, String name,
 
                             @NotBlank
                             @Pattern(regexp = "^COREN-[A-Z]{2} \\d{4,6}-(ENF|TE|AE)$", message = "Formato de COREN inválido. Ex: COREN-SP 123456-ENF")
