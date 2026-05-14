27267b5dd9d116be3b4a81515bedd22177091812
CarlosZeyy <carloszeeyy@gmail.com>
2026-05-14 08:58:19 -0300

    feat: implementing refresh token

--- src/main/java/dev/carlosmoises/projeto/enferm/service/TokenService.java
+++ src/main/java/dev/carlosmoises/projeto/enferm/service/TokenService.java
@@ -2,14 +2,23 @@ package dev.carlosmoises.projeto.enferm.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import dev.carlosmoises.projeto.enferm.model.RefreshToken;
 import dev.carlosmoises.projeto.enferm.model.User;
+import dev.carlosmoises.projeto.enferm.repository.RefreshTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 @Service
 public class TokenService {
+
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+
     public String generateToken(User user) {
         String passwordSecret = System.getenv("PASSWORD_SECRET");
         Algorithm algorithm = Algorithm.HMAC256(passwordSecret);
@@ -18,7 +27,7 @@ public class TokenService {
         Instant tokenExpired = tokenNow.plus(2, ChronoUnit.HOURS);
 
         return JWT.create()
-                .withIssuer("API Enfermagem")
+                .withIssuer("API EnfermeirEX")
                 .withSubject(user.getEmail())
                 .withExpiresAt(tokenExpired)
                 .sign(algorithm);
@@ -30,12 +39,21 @@ public class TokenService {
             Algorithm algorithm = Algorithm.HMAC256(System.getenv("PASSWORD_SECRET"));
 
             return JWT.require(algorithm)
-                    .withIssuer("API Enfermagem")
+                    .withIssuer("API EnfermeirEX")
                     .build()
                     .verify(tokenJWT)
                     .getSubject();
         } catch (Exception exception) {
             throw new RuntimeException("Token JWT inválido ou expirado");
         }
+    }
+
+    public RefreshTokenRepository createRefreshToken(User user) {
+        var refreshToken = new RefreshToken();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setId(user.getId());
+        refreshToken.setExpiryDate(LocalDateTime.now().plusDays(14));
+
+        return refreshTokenRepository.save(refreshToken);
     }
 }
